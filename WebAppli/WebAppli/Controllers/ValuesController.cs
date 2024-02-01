e60955b14f10d1f70fee81ba88044789e36cb2fb
LoaKenpf <theo.casella38@gmail.com>
2024-02-01 11:34:24 +0100

    pushEnLegende

---
+++ WebAppli/WebAppli/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppli.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+    }
+}
---
+++ WebAppli/WebAppli/Models/FileName.cs
@@ -0,0 +1,16 @@
+﻿namespace Api_Papeterie.View_models
+{
+    public class Papeterie
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+        public required string Texture { get; set; }
+
+        public string? Grammage { get; set; }
+
+        public int? Prix { get; set; }
+
+        public string? Couleur { get; set; }
+    }
+}
